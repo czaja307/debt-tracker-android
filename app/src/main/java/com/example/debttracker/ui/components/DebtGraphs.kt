788456588c275349b60a86cd10fde918c7c5d3f6
Jakub Czajkowski <38287974+czaja307@users.noreply.github.com>
2025-04-16 11:15:37 +0200

    perf: remove blocking execution

--- app/src/main/java/com/example/debttracker/ui/components/DebtGraphs.kt
+++ app/src/main/java/com/example/debttracker/ui/components/DebtGraphs.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -95,7 +96,7 @@ private val dataLineGraph = mapOf(
 fun DebtOverTimeGraph() {
     val modelProducer = remember { CartesianChartModelProducer() }
 
-    runBlocking {
+    LaunchedEffect(Unit) {
         modelProducer.runTransaction {
             lineSeries { series(dataLineGraph.values) }
             extras { it[BottomAxisLabelKey] = dataLineGraph.keys.toList() }
